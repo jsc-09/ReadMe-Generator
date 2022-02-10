9e24a8b772b3ee6fec744270255c63ea4f3fddfd
Joanna Christian <joanna.s.christian@gmail.com>
2022-02-09 21:57:10 -0800

    created read me section prompts

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
-let inquirer = require('inquirer')
-let fs = require('fs')
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Project Title',
+            name: 'projectTitle'
+        },
+        {
+            type: 'input',
+            message: 'Description of the Project',
+            name: 'description', 
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions',
+            name: 'instructions', 
+        },
+        {
+            type: 'input',
+            message: 'Usage Information',
+            name: 'usage', 
+        },
+        {
+            type: 'input',
+            message: 'Contribution Guidelines',
+            name: "contribution", 
+        },
+        {
+            type: 'input',
+            message: 'Test Instructions',
+            name: 'test', 
+        },
+        {
+            type: 'list',
+            message: 'Type of license',
+            name: 'license',
+            choices: ['Apache','MIT', 'ISC','BSD-New', 'FreeBSD','GPL', 'LGPL','Mozilla', 'Eclipse','CDDL'] 
+        },
+        {
+            type: 'input',
+            message: 'Github URL',
+            name: 'github', 
+        },
+        {
+            type: 'input',
+            message: 'Your Email',
+            name: 'email', 
+        },
+
+    ])
+
+.then((answers) => {
+    console.log(answers);
+});
